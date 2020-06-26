0ae85156181298eb1c12c9c0956a1ac41fd3d954
Carmelo Evoli <carmelo.evoli@gssi.it>
2020-06-26 17:46:15 +0200

    minor changes

--- examples/frag_table.cpp
+++ examples/frag_table.cpp
@@ -3,29 +3,26 @@
 #include <fstream>
 #include <iomanip>
 #include <limits>
+#include <memory>
 #include <string>
 #include <vector>
 
 #include "XS4GCR/pid.h"
 #include "XS4GCR/xs4gcr.h"
 
-void write_table(const std::vector<XS4GCR::PID>& particles, const std::string& filename,
+void write_table(const std::shared_ptr<XS4GCR::Spallation>& sigma,
+                 const std::shared_ptr<XS4GCR::CosmicRayChart>& chart, const std::string& filename,
                  bool doGhosts) {
-    XS4GCR::XSECS xsec;
-    xsec.set_secondary_nuclei("Evoli2019");
-    auto x_in = xsec.create_secondary_nuclei();
-
     XS4GCR::TARGET H_ISM(1);
 
     std::fstream txtfile;
     txtfile.open(filename, std::ios_base::out);
     txtfile.precision(3);
 
-    double E_min = 10. * MKS::MeV;
-    double E_max = 1. * MKS::TeV;
-    size_t E_size = 5 * 32;
-
-    XS4GCR::log_axis<double> E(E_min, E_max, E_size);
+    const double E_min = 10. * MKS::MeV;
+    const double E_max = 1. * MKS::TeV;
+    const size_t E_size = 5 * 32;
+    const XS4GCR::log_axis<double> E(E_min, E_max, E_size);
 
     txtfile << "0 0 0 0 ";
     for (size_t i = 0; i < E_size; ++i) {
@@ -34,7 +31,9 @@ void write_table(const std::vector<XS4GCR::PID>& particles, const std::string& f
     }
     txtfile << "\n";
 
-    int counter = 0;
+    auto particles = chart->get_particle_list();
+
+    size_t counter = 0;
     for (auto& projectile : particles) {
         for (auto& fragment : particles) {
             if (projectile.get_A() > fragment.get_A()) {
@@ -43,7 +42,7 @@ void write_table(const std::vector<XS4GCR::PID>& particles, const std::string& f
                 txtfile << projectile.get_Z() << " " << projectile.get_A() << " ";
                 for (size_t i = 0; i < E_size; ++i) {
                     XS4GCR::channel ch(projectile, fragment);
-                    txtfile << x_in->get(ch, H_ISM, E.at(i), doGhosts) / MKS::mbarn;
+                    txtfile << sigma->get(ch, H_ISM, E.at(i), doGhosts) / MKS::mbarn;
                     if (i < E_size - 1) txtfile << " ";
                 }
                 txtfile << "\n";
@@ -54,23 +53,13 @@ void write_table(const std::vector<XS4GCR::PID>& particles, const std::string& f
 }
 
 int main() {
-    std::vector<XS4GCR::PID> particles;
+    XS4GCR::XSECS xsec;
 
-    std::string filename = "data/crchart_Z28.txt";
-    std::ifstream infile(filename.c_str());
-    infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    int Z;
-    int A;
-    while (!infile.eof()) {
-        infile >> Z;
-        infile >> A;
-        infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        auto pid = XS4GCR::PID(Z, A);
-        particles.push_back(pid);
-    }
-    std::cout << " - read my particle chart with " << particles.size() << " nuclei\n";
+    xsec.set_secondary_nuclei("Evoli2019");
+    auto x_in = xsec.create_secondary_nuclei();
 
-    write_table(particles, "crxsecs_fragmentation_Evoli2019_direct.txt", false);
-    write_table(particles, "crxsecs_fragmentation_Evoli2019_cumulative.txt", true);
-    return 0;
+    auto crchart = xsec.create_decay_chart();
+
+    write_table(x_in, crchart, "crxsecs_fragmentation_Evoli2019_direct.txt", false);
+    write_table(x_in, crchart, "crxsecs_fragmentation_Evoli2019_cumulative.txt", true);
 }
