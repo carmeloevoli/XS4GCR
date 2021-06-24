b5f5410aa147246b71aa0ebd5c6222a108ac9cb5
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-06-24 13:59:47 +0200

    added nuclei names

---
+++ include/XS4GCR/nucleinames.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2017 Carmelo Evoli - MIT License
+#ifndef INCLUDE_XS4GCR_NUCLEILIST_H_
+#define INCLUDE_XS4GCR_NUCLEILIST_H_
+
+#include "XS4GCR/pid.h"
+
+namespace XS4GCR {
+
+static const auto Be7 = XS4GCR::PID(4, 7);
+static const auto Be9 = XS4GCR::PID(4, 9);
+static const auto Be10 = XS4GCR::PID(4, 10);
+static const auto B10 = XS4GCR::PID(5, 10);
+static const auto B11 = XS4GCR::PID(5, 11);
+static const auto C11 = XS4GCR::PID(6, 11);
+static const auto C12 = XS4GCR::PID(6, 12);
+
+}  // namespace XS4GCR
+
+#endif
