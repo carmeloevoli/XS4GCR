12f7d2316a59556101ce8a6a24fe4271730f46fb
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-06-26 16:55:09 +0200

    mv particlenames file

--- include/XS4GCR/particlenames.h
+++ include/XS4GCR/particlenames.h
@@ -6,6 +6,14 @@
 
 namespace XS4GCR {
 
+static const auto electron = XS4GCR::PID(-1, 0);
+static const auto positron = XS4GCR::PID(1, 0);
+static const auto H1 = XS4GCR::PID(1, 1);
+static const auto H2 = XS4GCR::PID(1, 2);
+static const auto He3 = XS4GCR::PID(2, 3);
+static const auto He4 = XS4GCR::PID(2, 4);
+static const auto Li6 = XS4GCR::PID(3, 6);
+static const auto Li7 = XS4GCR::PID(3, 7);
 static const auto Be7 = XS4GCR::PID(4, 7);
 static const auto Be9 = XS4GCR::PID(4, 9);
 static const auto Be10 = XS4GCR::PID(4, 10);
@@ -13,6 +21,9 @@ static const auto B10 = XS4GCR::PID(5, 10);
 static const auto B11 = XS4GCR::PID(5, 11);
 static const auto C11 = XS4GCR::PID(6, 11);
 static const auto C12 = XS4GCR::PID(6, 12);
+
+static const auto H_ISM = TARGET(TARGET::GasType::H);
+static const auto He_ISM = TARGET(TARGET::GasType::He);
 
 }  // namespace XS4GCR
 
